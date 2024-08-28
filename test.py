5d05445af6642181d86bf45fd32283f6382c3f48
B.M. Nakib Borshon <nakibborshon604@gmail.com>
2024-08-28 16:40:58 +0600

    chnages3

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
+import math;
+
 print('Hello')
 print('Hello world')
+
+def add(a,b):
+    result = a+b
+    print("sum of is:",result)
+
+x = int(input('Enter a num')) 
+y = int(input('Enter a  2nd num'))
+
+add(x,y)
